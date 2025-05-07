7c32e3ffd42f19957ee922d6124f8a491ef4ee8e
salena123 <sasmurka@mail.ru>
2025-05-08 02:01:44 +0500

    .

--- app/src/main/java/com/example/android_practice/listWithDetails/presentation/screens/SelectionDialog.kt
+++ app/src/main/java/com/example/android_practice/listWithDetails/presentation/screens/SelectionDialog.kt
@@ -1,97 +1,97 @@
-package com.example.android_practice.listWithDetails.presentation.screens
-
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import com.example.android_practice.listWithDetails.domain.entity.DogShortEntity
-import com.example.android_practice.ui.Spacing
-
-@Preview
-@Composable
-fun SelectionDialogPreview() {
-    SelectionDialog(
-        onDismissRequest = {},
-        onConfirmation = {},
-        title = "Тип",
-        variants = setOf(DogShortEntity, DogType.SERIES),
-        selectedVariants = setOf(MovieType.SERIES),
-        onVariantSelectedChanged = { _, _ -> }
-    )
-}
-
-@Composable
-fun SelectionDialog(
-    onDismissRequest: () -> Unit,
-    onConfirmation: () -> Unit,
-    title: String,
-    variants: Set<DogShortEntity>,
-    selectedVariants: Set<DogShortEntity>,
-    onVariantSelectedChanged: (DogShortEntity, Boolean) -> Unit
-) {
-    Dialog(onDismissRequest = { onDismissRequest() }) {
-        // Draw a rectangle shape with rounded corners inside the dialog
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(Spacing.medium),
-            shape = RoundedCornerShape(16.dp),
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                verticalArrangement = Arrangement.Center,
-            ) {
-                Text(
-                    text = title,
-                    modifier = Modifier.padding(16.dp),
-                    style = MaterialTheme.typography.titleMedium
-                )
-
-                variants.forEach { variant ->
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Checkbox(
-                            checked = variant in selectedVariants,
-                            onCheckedChange = { onVariantSelectedChanged(variant, it) }
-                        )
-                        Text(text = stringResource(id = variant.stringRes))
-                    }
-                }
-
-
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End,
-                ) {
-                    TextButton(
-                        onClick = { onDismissRequest() },
-                        modifier = Modifier.padding(Spacing.small),
-                    ) {
-                        Text("Dismiss")
-                    }
-                    TextButton(
-                        onClick = { onConfirmation() },
-                        modifier = Modifier.padding(Spacing.small),
-                    ) {
-                        Text("Confirm")
-                    }
-                }
-            }
-        }
-    }
-}
+//package com.example.android_practice.listWithDetails.presentation.screens
+//
+//import androidx.compose.foundation.layout.Arrangement
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.Row
+//import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.foundation.shape.RoundedCornerShape
+//import androidx.compose.material3.Card
+//import androidx.compose.material3.Checkbox
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Text
+//import androidx.compose.material3.TextButton
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.res.stringResource
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.window.Dialog
+//import com.example.android_practice.listWithDetails.domain.entity.DogShortEntity
+//import com.example.android_practice.ui.Spacing
+//
+//@Preview
+//@Composable
+//fun SelectionDialogPreview() {
+//    SelectionDialog(
+//        onDismissRequest = {},
+//        onConfirmation = {},
+//        title = "Тип",
+//        variants = setOf(DogShortEntity, DogType.SERIES),
+//        selectedVariants = setOf(MovieType.SERIES),
+//        onVariantSelectedChanged = { _, _ -> }
+//    )
+//}
+//
+//@Composable
+//fun SelectionDialog(
+//    onDismissRequest: () -> Unit,
+//    onConfirmation: () -> Unit,
+//    title: String,
+//    variants: Set<DogShortEntity>,
+//    selectedVariants: Set<DogShortEntity>,
+//    onVariantSelectedChanged: (DogShortEntity, Boolean) -> Unit
+//) {
+//    Dialog(onDismissRequest = { onDismissRequest() }) {
+//        // Draw a rectangle shape with rounded corners inside the dialog
+//        Card(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(Spacing.medium),
+//            shape = RoundedCornerShape(16.dp),
+//        ) {
+//            Column(
+//                modifier = Modifier
+//                    .fillMaxWidth(),
+//                verticalArrangement = Arrangement.Center,
+//            ) {
+//                Text(
+//                    text = title,
+//                    modifier = Modifier.padding(16.dp),
+//                    style = MaterialTheme.typography.titleMedium
+//                )
+//
+//                variants.forEach { variant ->
+//                    Row(verticalAlignment = Alignment.CenterVertically) {
+//                        Checkbox(
+//                            checked = variant in selectedVariants,
+//                            onCheckedChange = { onVariantSelectedChanged(variant, it) }
+//                        )
+//                        Text(text = stringResource(id = variant.stringRes))
+//                    }
+//                }
+//
+//
+//                Row(
+//                    modifier = Modifier
+//                        .fillMaxWidth(),
+//                    horizontalArrangement = Arrangement.End,
+//                ) {
+//                    TextButton(
+//                        onClick = { onDismissRequest() },
+//                        modifier = Modifier.padding(Spacing.small),
+//                    ) {
+//                        Text("Dismiss")
+//                    }
+//                    TextButton(
+//                        onClick = { onConfirmation() },
+//                        modifier = Modifier.padding(Spacing.small),
+//                    ) {
+//                        Text("Confirm")
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
